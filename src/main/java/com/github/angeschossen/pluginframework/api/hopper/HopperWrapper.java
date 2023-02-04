255ceba06b8c937cdc7755801bc1f4ad413a09b8
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2023-02-04 18:16:46 +0100

    hopper item delete

--- src/main/java/com/github/angeschossen/pluginframework/api/hopper/HopperWrapper.java
+++ src/main/java/com/github/angeschossen/pluginframework/api/hopper/HopperWrapper.java
@@ -2,12 +2,13 @@ package com.github.angeschossen.pluginframework.api.hopper;
 
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 
 public interface HopperWrapper {
-    @NotNull
-    HashMap<Integer, ItemStack> addItem(ItemStack... item);
+
+    @Nullable ItemStack addItem(ItemStack item, boolean delete);
 
     boolean isFull();
 }
