992a1b622b0898dfe2816c47c8a3b0af80f6adc2
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2023-08-24 14:56:50 +0200

    upd

--- src/main/java/com/github/angeschossen/pluginframework/api/utils/Checks.java
+++ src/main/java/com/github/angeschossen/pluginframework/api/utils/Checks.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 public class Checks {
 
-    public static void nonNull(Object o, String key) {
+    public static void requireNonNull(Object o, String key) {
         Objects.requireNonNull(o, key + " can't be null");
     }
 }
