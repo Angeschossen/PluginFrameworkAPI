1ddddbf6d1ea383b96e5f184377616893c1c833c
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2024-07-14 16:18:00 +0200

    Config

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ java {
 }
 
 group = "com.github.angeschossen"
-version = "1.0.25"
+version = "1.0.26"
 description = "PluginFrameworkAPI"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
---
+++ src/main/java/com/github/angeschossen/pluginframework/api/locale/Environment.java
@@ -0,0 +1,5 @@
+package com.github.angeschossen.pluginframework.api.locale;
+
+public enum Environment {
+    MINECRAFT, DISCORD
+}
